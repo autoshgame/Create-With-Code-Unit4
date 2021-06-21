dcb319c262da8d99fccbc049b68205096e2b684b
autoshgame <66913909+autoshgame@users.noreply.github.com>
2021-06-21 16:22:42 +0700

    Complete lesson 4.1 < Descriptions are below >
    - Create project and open scene
    - Set up the player and add a texture
    - Create a focal point for the camera
    - Rotate the focal point by user input
    - Add forward force to the player
    - Move in direction of focal point

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] protected float speed;
+
+    protected Rigidbody playerRB;
+
+    protected GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = this.GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fowardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * speed * fowardInput, ForceMode.Impulse);
+    }
+
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] protected float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        this.transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
