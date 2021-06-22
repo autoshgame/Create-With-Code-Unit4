4a5cd795adeae8faa47f0cf41f4bb044fb908825
autoshgame <66913909+autoshgame@users.noreply.github.com>
2021-06-22 11:25:31 +0700

    Complete lesson 4.2(Follow the player) <Descriptions are below>
    Works have done :
    - Add an enemy and a physics material
    - Create enemy script to follow player
    - Create a lookDirection variable
    - Create a Spawn Manager for the enemy
    - Randomly generate spawn position
    - Make a method return a spawn point

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float speed;
+
+    private GameObject player;
+
+    private Rigidbody enemyRB;
+
+    // Start is called before the first frame update
+    [SerializeField]
+    protected void Start()
+    {
+        enemyRB = this.GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirectiion = (player.transform.position - this.transform.position).normalized;
+        enemyRB.AddForce(lookDirectiion * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] float spawnRange = 9f;
+
+    [SerializeField] protected GameObject enemyPrefabs;
+
+    private Vector3 GenerateRandomPosition()
+    {
+        Vector3 randomPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+        return randomPos;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Instantiate(enemyPrefabs, GenerateRandomPosition(), enemyPrefabs.transform.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
